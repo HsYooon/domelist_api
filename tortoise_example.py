852dd1439302632673d2fa75c866a08b490bedcd
HsYooon <sssu0000@gmail.com>
2022-11-28 18:02:17 +0900

    db 정보 삭제

--- tortoise_example.py
+++ tortoise_example.py
@@ -4,7 +4,7 @@ from tortoise.models import Model
 async def init():
     # db_url 예시 => {DB_TYPE}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}?{PARAM1}=value&{PARAM2}=value
     await Tortoise.init(
-        db_url='mysql://user:wjdgns001!A@203.245.41.222:3306/domelist',
+        db_url='',
         modules={'models': ['models']}
     )
     # Generate the schema
