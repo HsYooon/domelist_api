dd989f3a84613fad26e613641d672a3756c084f6
HsYooon <sssu0000@gmail.com>
2022-11-30 17:49:13 +0900

    :zap: add cnt var, connect route to api

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from time import sleep
 from flask import Flask, render_template, jsonify
 import json
 import urllib3
@@ -25,11 +26,28 @@ def hello(name=None):
 @app.route('/imweb/categories')
 def categories():
     result = api.categories()
+    request_cnt = 0
+    for i in result:
+        if request_cnt >= 5:
+            sleep(1)
+            request_cnt = 0
+        content = f'<div style="text-align: center;"><h3>[{i["name"]}] - {i["title"]} </h3><h4 style="color: orange;">&#11015; 클릭시 해당 도매사이트로 이동합니다</h4><h4><a href="{i["url"]}" style="text-decoration: underline;"target="_blank">상세보기</a></h4></div>'
+        api.register_prodect(i['category_name'], i['img'], i['title'], content, content, i['info'])
+        request_cnt += 1
+
     return result
 
 @app.route('/imweb/regist_product')
 async def regist_product():
-    result = await dao.test()
+    result = await dao.products()
+    request_cnt = 0
+    for i in result:
+        if request_cnt >= 5:
+            sleep(1)
+            request_cnt = 0
+        content = f'<div style="text-align: center;"><h3>[{i["name"]}] - {i["title"]} </h3><h4 style="color: orange;">&#11015; 클릭시 해당 도매사이트로 이동합니다</h4><h4><a href="{i["url"]}" style="text-decoration: underline;"target="_blank">상세보기</a></h4></div>'
+        api.register_prodect(i['category_name'], i['img'], i['title'], content, content, i['info'])
+        request_cnt += 1
     return jsonify(result)
 
 @app.route('/imweb/delete_product/<id>')
